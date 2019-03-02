af3c346d4b262c1469b39511b88830da8f5b8a8d
bhlx <hellerbenjamin@gmx.de>
2019-03-02 11:13:21 +0100

    test gpio

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module sensorgo
 
-require github.com/gorilla/mux v1.7.0
+require (
+	github.com/gorilla/mux v1.7.0
+	github.com/stianeikeland/go-rpio v4.2.0+incompatible // indirect
+)
--- main.go
+++ main.go
@@ -2,19 +2,37 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/gorilla/mux"
+	"fmt"
+	"github.com/stianeikeland/go-rpio"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 
 	// Setup a mux router
-	router := mux.NewRouter()
+	// router := mux.NewRouter()
 
-	router.HandleFunc("/api/say-hello", SayHello).Methods("GET")
+	// router.HandleFunc("/api/say-hello", SayHello).Methods("GET")
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	// log.Fatal(http.ListenAndServe(":8080", router))
+
+	fmt.Println("opening gpio")
+
+	if err := rpio.Open(); err != nil {
+		log.Fatal("unable to open gpio", err)
+	}
+
+	defer rpio.Close()
+
+	pin := rpio.Pin(18)
+	pin.Output()
+
+	for x := 0; x < 20; x++ {
+		pin.Toggle()
+		time.Sleep(time.Second / 5)
+	}
 }
 
 func SayHello(w http.ResponseWriter, r *http.Request) {
